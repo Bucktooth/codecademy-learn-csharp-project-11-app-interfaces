32f33efd931a377691e7e3b41687a8f3a1d6fbe6
Joey Troxclair <viiwebhead@live.com>
2021-12-08 18:12:18 -0600

    Create TodoList.cs

---
+++ TodoList.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace SavingInterface
+{
+  class TodoList : IDisplayable, IResetable
+  {
+    public string[] Todos
+    { get; private set; }
+
+    private int nextOpenIndex;
+
+    public TodoList()
+    {
+      Todos = new string[5];
+      nextOpenIndex = 0;
+    }
+
+    public void Add(string todo)
+    {
+      Todos[nextOpenIndex] = todo;
+      nextOpenIndex++;
+    }
+
+    public void Display()
+    {
+      foreach (string s in Todos)
+      {
+        Console.WriteLine(s);
+      }
+    }
+
+    public void Reset()
+    {
+      Todos = new string[5];
+      nextOpenIndex = 0;
+    }
+  }
+}
